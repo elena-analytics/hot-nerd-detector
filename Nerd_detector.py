e7f68c57f3a193c8ae7ee6a07bf106c6b02bb1bb
Elena <elena.michelli@gmail.com>
2026-03-06 23:02:02 -0500

    Add weighted nerd detection script

---
+++ Nerd_detector.py
@@ -0,0 +1,28 @@
+import random
+
+traits = {
+    "tall": 1,
+    "lanky": 1,
+    "glasses": 2,
+    "nerd_vibe": 3,
+    "cute_face": 1,
+    "single": 2
+}
+
+def generate_person():
+    return {trait: random.choice([True, False]) for trait in traits}
+
+def evaluate(person):
+    score = sum(traits[t] for t in person if person[t])
+
+    if score >= 7:
+        return "Category C: Elite nerd specimen"
+    elif score >= 4:
+        return "Category B: Promising nerd"
+    else:
+        return "Category A: False alarm"
+
+person = generate_person()
+
+print(person)
+print(evaluate(person))
